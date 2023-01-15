280930128735954513b3171208cf8569f8207748
Anton Khodasevich <khodasevich.anton.eduardovich@gmail.com>
2023-01-15 23:15:41 +0300

    change comments

--- src/system/config.sample.php
+++ src/system/config.sample.php
@@ -2,12 +2,12 @@
 
 $config = [
   'amoCRM' => [
-    'subdomain'     => '',                  // пример: kislovodskayak'
-    'client_secret' => '',                  // пример: D9aZ6vOhhIPrxfp53PgYYwtnVTic6ddsNSB4Lbcefh5ysyelz30wFbTd3canXv3N'
-    'client_id'     => '',                  // пример: 2e7af213-9172-4147-331e-c112a36ae8a3'
-    'code'          => '',                  // пример: def501005cacdfa5f18aa1329c8413611b3b1ca2fabb10fe024203ebec20421275e6de655cff5e211205e5a2e31e29c40b344debcdd0ddfa227c7cfad7ed9665f719fa3c645e659c09e9086a69a018c08c98ef26145249878ae9adcf8382e78da2bb73d9996a512856981a190a6edcfec9272105f302fe7668c4c6f8d7b935b0a5b3aa6e57a2f416dcab8a43d815f9ddb7d2666d7a42534bd576b3cbca1de74ad93e3eedf6f4f04d7c9489ad6e109953de07b345c4cf55f81b456b191f65b4eb023fcb4dda3afee9bdfaac7c043563489751935756ba3a1a029f9d8779e08f5ae322b709d59179ab834378289336ea758c4911fc506ef48085105fac0ef25d52a616652bc3cd82993d39552643e9c085d1d25fb572ab425433c06890d3ed6bf4c4d377ff92b956d37ccb73cd29a855fab7d0bcf8afd051f31cff645afa1b401828a1444806cffdb289940826c9679e906a705e60d35f2ca88f77f823a80fb5565dd2b892d994e87f84f94e6c586f1532a7d578e2aba36afaaad519f22eba4a7def5e19bae8a16f813e1fcd3f90c8643cdd63278943ed2b731d9715ea86d6db312275be86414a1f41cd8fb24717f17d1da30dbe27bb22cab1c91cfe8835e8f314f08b6775e52269ad0f8f6cfcd3c2cc189a96b06da124a971fdcdeed8149160da070133d4bba38d07a8b9d6165bf05ac8ffea6080ec70bc712f00bdf4b2462b1f'
+    'subdomain'     => '',                  // пример: kislovodskayak
+    'client_secret' => '',                  // пример: D9aZ6vOhhIPrxfp53PgYYwtnVTic6ddsNSB4Lbcefh5ysyelz30wFbTd3canXv3N
+    'client_id'     => '',                  // пример: 2e7af213-9172-4147-331e-c112a36ae8a3
+    'code'          => '',                  // пример: def501005cacdfa5f18aa1329c8413611b3b1ca2fabb10f...
     'token_file'    => 'tokens.txt',
-    'redirect_uri'  => '',                  // пример: http://n92361h9.beget.tech/system/library/amocrm/amocrm.php'
+    'redirect_uri'  => '',                  // пример: http://n92361h9.beget.tech/system/library/amocrm/amocrm.php
   ],
   'mail' => [
     'protocol' => '',                       // пример: mail || smtp
